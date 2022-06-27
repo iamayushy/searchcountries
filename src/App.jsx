c4f77d3213919f65c410b320f8d215ece90e3097
iamayushy <ayushhuns@gmail.com>
2022-06-27 13:23:45 +0530

    bugged the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Search } from './components/Search'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      ss
+      <Search/>
     </div>
   )
 }
